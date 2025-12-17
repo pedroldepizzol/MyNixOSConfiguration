a84620ab7682cda190f440dfbce34cf8a792848e
Chester-Berkeley <151783746+pedroldepizzol@users.noreply.github.com>
2025-12-17 09:54:07 -0300

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -15,22 +15,14 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  # If the ID doesn't work, uncomment the code below.
-  #networking.networkmanager.enable = true;
-  networking = {
-    networkmanager = {
-      enable = true;
-      wifi.backend = "iwd";
-    };
-  };
-
+  networking.networkmanager.enable = true;
+  
   # Set your time zone.
   time.timeZone = "America/Sao_Paulo";
 
